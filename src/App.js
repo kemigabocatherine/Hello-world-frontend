d89ac8f3dc3b840f90e9113366002cd6bfe01761
Catherine Kemigabo <kemigabocatherine205@gmail.com>
2022-10-07 00:44:13 +0300

    Add api request

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import Greetings from './components/greetings';
+
+const API_URL = 'http://localhost:3000/api/v1/greetings';
+
+function getApiData() {
+  return axios.get(API_URL).then((response) => response.data)
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
     </div>
   );
 }
